e9fd64481052b557757a50705e4f898fc9727535
Aigerim Omirzak <omirzaq@kazpost.kz>
2024-02-27 16:39:59 +0600

    add mobile layout for main page

--- src/routes/Main/Main.jsx
+++ src/routes/Main/Main.jsx
@@ -18,11 +18,14 @@ import {
   blueCheck,
 } from "../../assets/images/index";
 
+import useIsMobile from "../../utils/useIsMobile";
+
 import styles from "./main.module.scss";
 
 export const Main = () => {
-  const { t } = useTranslation('main');
+  const { t } = useTranslation("main");
   const navigate = useNavigate();
+  const isMobile = useIsMobile();
 
   const cardData = [
     {
@@ -64,14 +67,38 @@ export const Main = () => {
   ];
 
   const cardData2 = [
-    { title: `${t("cardData2Title1")}`, subtitle: `${t("cardData2Subtitle1")}` },
-    { title: `${t("cardData2Title2")}`, subtitle: `${t("cardData2Subtitle2")}` },
-    { title: `${t("cardData2Title3")}`, subtitle: `${t("cardData2Subtitle3")}` },
-    { title: `${t("cardData2Title4")}`, subtitle: `${t("cardData2Subtitle4")}` },
-    { title: `${t("cardData2Title5")}`, subtitle: `${t("cardData2Subtitle5")}` },
-    { title: `${t("cardData2Title6")}`, subtitle: `${t("cardData2Subtitle6")}` },
-    { title: `${t("cardData2Title7")}`, subtitle: `${t("cardData2Subtitle7")}` },
-    { title: `${t("cardData2Title8")}`, subtitle: `${t("cardData2Subtitle8")}` },
+    {
+      title: `${t("cardData2Title1")}`,
+      subtitle: `${t("cardData2Subtitle1")}`,
+    },
+    {
+      title: `${t("cardData2Title2")}`,
+      subtitle: `${t("cardData2Subtitle2")}`,
+    },
+    {
+      title: `${t("cardData2Title3")}`,
+      subtitle: `${t("cardData2Subtitle3")}`,
+    },
+    {
+      title: `${t("cardData2Title4")}`,
+      subtitle: `${t("cardData2Subtitle4")}`,
+    },
+    {
+      title: `${t("cardData2Title5")}`,
+      subtitle: `${t("cardData2Subtitle5")}`,
+    },
+    {
+      title: `${t("cardData2Title6")}`,
+      subtitle: `${t("cardData2Subtitle6")}`,
+    },
+    {
+      title: `${t("cardData2Title7")}`,
+      subtitle: `${t("cardData2Subtitle7")}`,
+    },
+    {
+      title: `${t("cardData2Title8")}`,
+      subtitle: `${t("cardData2Subtitle8")}`,
+    },
   ];
 
   const mainTextData = [
@@ -82,12 +109,115 @@ export const Main = () => {
     { text: `${t("mainText5")}` },
   ];
 
-
-  return (
+  return isMobile ? (
+    <div className={styles.mobileContainer}>
+      <div className={styles.section1}>
+        <div className={styles.contents}>
+          <h2>
+            <TypingAnimation text={t("section1.title")} speed={100} />
+          </h2>
+          <p>{t("section1.description")}</p>
+        </div>
+      </div>
+      <div className={styles.section2}>
+        <div className={styles.mainContent}>
+          <div className={styles.topText}>
+            <h2>{t("section2.title")}</h2>
+            <p>{t("section2.description")}</p>
+          </div>
+          <div className={styles.secondaryContent}>
+            <div className={styles.img1Container}>
+              <img src={uni1} alt="uni1" className={styles.img1} />
+            </div>
+            <div className={styles.cardContainer}>
+              {cardData2.map((item, index) => (
+                <div className={styles.card} key={index}>
+                  <p className={styles.title}>{item.title}</p>
+                  <p className={styles.subtitle}>{item.subtitle}</p>
+                </div>
+              ))}
+            </div>
+            <div className={styles.img2Container}>
+              <img src={uni2} alt="uni2" className={styles.img2} />
+              <p>{t("section2.description")}</p>
+              <button
+                type="button"
+                className={styles.btn1}
+                onClick={() => {
+                  navigate("/academics");
+                }}
+              >
+                {t("readmore")}
+              </button>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div className={styles.section3}>
+        <div className={styles.mainContent}>
+          <div className={styles.topText}>
+            <h2>{t("section3.title")}</h2>
+            <p>{t("section3.description")}</p>
+          </div>
+          <div className={styles.cardContainer}>
+            {cardData.map((item, index) => (
+              <div className={styles.cardItem} key={index}>
+                <img
+                  src={item.imgSrc}
+                  alt={item.altText}
+                  className={styles.cardItemImg}
+                />
+                <div className={styles.cardItemText}>
+                  <p className={styles.title}>{item.title}</p>
+                  <p className={styles.subtitle}>{item.subtitle}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={styles.section4}>
+        <div className={styles.mainContent}>
+          <div className={styles.topText}>
+            <h2>{t("section4.title")}</h2>
+            <p>{t("section4.description")}</p>
+          </div>
+          <div className={styles.secondaryContent}>
+            <img src={studentConsult} alt="students" className={styles.img1} />
+            <div className={styles.textContent}>
+              {mainTextData.map((item, index) => (
+                <div className={styles.mainText} key={index}>
+                  <img
+                    src={blueCheck}
+                    alt="blueCheck"
+                    className={styles.checkImg}
+                  />
+                  <p className={styles.subtext}>{item.text}</p>
+                </div>
+              ))}
+              <button
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  navigate("/admissions");
+                }}
+              >
+                {t("seeAll")}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  ) : (
     <div className={styles.container}>
       <div className={styles.section1}>
         <div className={styles.contents}>
-          <h2><TypingAnimation text={t("section1.title")} speed={100} /></h2>
+          <h2>
+            <TypingAnimation text={t("section1.title")} speed={100} />
+          </h2>
           <p>{t("section1.description")}</p>
         </div>
       </div>
@@ -103,10 +233,14 @@ export const Main = () => {
             </div>
             <div className={styles.img2Container}>
               <img src={uni2} alt="uni2" className={styles.img2} />
-              <p>
-                {t("section2.description")}
-              </p>
-              <button type="button" className={styles.btn1} onClick={()=>{navigate('/academics')}}>
+              <p>{t("section2.description")}</p>
+              <button
+                type="button"
+                className={styles.btn1}
+                onClick={() => {
+                  navigate("/academics");
+                }}
+              >
                 {t("readmore")}
               </button>
             </div>
@@ -163,8 +297,14 @@ export const Main = () => {
                   <p className={styles.subtext}>{item.text}</p>
                 </div>
               ))}
-              <button type="button" className={styles.btn} onClick={()=>{navigate('/admissions')}}>
-                {t('seeAll')}
+              <button
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  navigate("/admissions");
+                }}
+              >
+                {t("seeAll")}
               </button>
             </div>
           </div>
